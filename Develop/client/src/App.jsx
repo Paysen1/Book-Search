ec484ebb000b5474a743678ccdecaa8f8dcb88f1
Peak <pglund18@yahoo.com>
2023-10-02 23:57:17 -0500

    launches no errors but white

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
-import ApolloClient from './apollo/client';
+import ApolloClient from '../node_modules/@apollo/client';
 import Navbar from './components/Navbar';
 
 function App() {
