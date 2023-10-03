44ef3c700fa2940e68d6809a39e60d8d36cb7ebd
Peak <pglund18@yahoo.com>
2023-10-02 23:07:00 -0500

    app, quieries and mutations done

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider } from '@apollo/client';
+import ApolloClient from './apollo/client';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={ApolloClient}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
